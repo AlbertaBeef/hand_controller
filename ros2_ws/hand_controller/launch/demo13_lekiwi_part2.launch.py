1ef849f864f9ed8843367adbc797c7bfef41f849
AlbertaBeef <grouby177@gmail.com>
2026-01-30 17:02:35 +0000

    ros2_ws: fix error in demo13_lekiwi_part2 launch script (remove joint_state_broadcaster, which generates conflicting data with twist_follower).

--- ros2_ws/hand_controller/launch/demo13_lekiwi_part2.launch.py
+++ ros2_ws/hand_controller/launch/demo13_lekiwi_part2.launch.py
@@ -176,13 +176,6 @@ def generate_launch_description():
         ],
     )
 
-    joint_state_broadcaster_spawner = Node(
-        package="controller_manager",
-        executable="spawner",
-        arguments=["joint_state_broadcaster"],
-        parameters=[{'use_sim_time': True}],
-    )
-
     omni_controllers_spawner = Node(
         package="controller_manager",
         executable="spawner",
@@ -230,7 +223,7 @@ def generate_launch_description():
         RegisterEventHandler(
             event_handler=OnProcessStart(
                 target_action=gz_bridge_node,
-                on_start=[joint_state_broadcaster_spawner, omni_controllers_spawner, joint_state_follower_node],
+                on_start=[omni_controllers_spawner, joint_state_follower_node],
             )
         )
     )
